04bc6614a035a458df94d3165722b7bcb340060f
Muhammad Haroon <mharoon1326@gmail.com>
2025-03-07 22:36:00 +0500

    `Added database configuration and initialization in config/database.go`

---
+++ config/database.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"log"
+	"os"
+	"pos-backend/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failedto connect to database:", err)
+	}
+
+	//  Auto-migrate tables
+	db.AutoMigrate(&models.User{}, &models.Product{}&models.Sale{})
+
+	DB = db
+}
